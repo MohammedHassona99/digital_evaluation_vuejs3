fde07de09e99c92dd3673c4e07e3498fb88c217e
MohammedHassona99 <mohammedhassona99@hotmail.com>
2023-09-13 17:54:25 +0300

    our-work-page

--- src/views/OurWorks.vue
+++ src/views/OurWorks.vue
@@ -5,103 +5,298 @@
     <div class="container">
       <div class="row g-md-2 g-0">
         <div class="col-6 mb-2 mb-md-0">
-          <div class="img" data-work="شاشة ويب">
+          <div
+            class="img"
+            data-work="شاشة ويب"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workOne.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6">
-          <div class="img" data-work="لموقع تصميم">
+          <div
+            class="img"
+            data-work="لموقع تصميم"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workTwo.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 mb-2 mb-md-0">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 d-none d-md-block">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 mb-2 mb-md-0">
-          <div class="img" data-work="شاشة ويب">
+          <div
+            class="img"
+            data-work="شاشة ويب"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workOne.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6">
-          <div class="img" data-work="لموقع تصميم">
+          <div
+            class="img"
+            data-work="لموقع تصميم"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workTwo.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 mb-2 mb-md-0">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 d-none d-md-block">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 mb-2 mb-md-0 d-md-none">
-          <div class="img" data-work="شاشة ويب">
+          <div
+            class="img"
+            data-work="شاشة ويب"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workOne.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 d-md-none">
-          <div class="img" data-work="لموقع تصميم">
+          <div
+            class="img"
+            data-work="لموقع تصميم"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workTwo.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 d-md-none mb-2 mb-md-0">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 d-md-none">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 col-md-4 gy-md-3 d-none d-md-block d-md-none">
-          <div class="img" data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول">
+          <div
+            class="img"
+            data-work="شاشة تسجيل الدخول يوجد بها معلومات الدخول"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropTow"
+          >
             <img src="./../assets/workThree.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 mb-2 mb-md-0 d-md-none">
-          <div class="img" data-work="شاشة ويب">
+          <div
+            class="img"
+            data-work="شاشة ويب"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workOne.png" class="img-fluid" alt="" />
           </div>
         </div>
         <div class="col-6 d-md-none">
-          <div class="img" data-work="لموقع تصميم">
+          <div
+            class="img"
+            data-work="لموقع تصميم"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
             <img src="./../assets/workTwo.png" class="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
+      <div
+        class="modal fade"
+        id="staticBackdropTow"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+            <div class="modal-header position-relative p-0">
+              <img
+                src="./../assets/model2.png"
+                width="1387px"
+                height="913px"
+                class="img-fluid"
+                alt=""
+              />
+              <button
+                type="button"
+                class="border-0 bg-transparent button-model"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+                <i class="fa-solid fa-circle-xmark text-danger text-opacity-75"></i>
+              </button>
+            </div>
+            <div class="modal-body">
+              <h4>تطبيق دلفري</h4>
+              <p>
+                هو التطبيق و الموقع الالكتروني المفضل للاكيلة في فلسطين <br />
+                و كمان اسهل و أبسط و اوفر طريقة تطلب أكلك من بين أكتر من<br />
+                200 مطعم في كل فلسطين. تشمل مطاعم
+              </p>
+              <h4>تطبيق دلفري</h4>
+              <p>
+                هو التطبيق و الموقع الالكتروني المفضل للاكيلة في فلسطين <br />
+                و كمان اسهل و أبسط و اوفر طريقة تطلب أكلك من بين أكتر من<br />
+                200 مطعم في كل فلسطين. تشمل مطاعم
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+            <div class="modal-header position-relative p-0">
+              <img
+                src="./../assets/model.png"
+                width="1387px"
+                height="913px"
+                class="img-fluid"
+                alt=""
+              />
+              <button
+                type="button"
+                class="border-0 bg-transparent button-model"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              >
+                <i class="fa-solid fa-circle-xmark text-danger text-opacity-75"></i>
+              </button>
+            </div>
+            <div class="modal-body">
+              <h4>تطبيق دلفري</h4>
+              <p>
+                هو التطبيق و الموقع الالكتروني المفضل للاكيلة في فلسطين <br />
+                و كمان اسهل و أبسط و اوفر طريقة تطلب أكلك من بين أكتر من<br />
+                200 مطعم في كل فلسطين. تشمل مطاعم
+              </p>
+              <h4>تطبيق دلفري</h4>
+              <p>
+                هو التطبيق و الموقع الالكتروني المفضل للاكيلة في فلسطين <br />
+                و كمان اسهل و أبسط و اوفر طريقة تطلب أكلك من بين أكتر من<br />
+                200 مطعم في كل فلسطين. تشمل مطاعم
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .img {
   position: relative;
-  &::before {
-    contain: attr(data-work);
+  &::after {
+    content: attr(data-work);
     position: absolute;
-    background-color: #6a6767;
+    background: rgba(109, 108, 108, 0.5);
     width: 100%;
     height: 100%;
+    top: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 25px;
+    display: none;
+  }
+  &:hover::after {
+    display: flex;
+  }
+}
+.button-model {
+  position: absolute;
+  top: 20px;
+  left: 25px;
+  i {
+    font-size: 35px;
   }
 }
 </style>
+// after in image is not ready
